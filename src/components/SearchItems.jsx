d98e57b95830d56a2d4284de1cf66736028d90c2
Fabio Marques <engfabiom@gmail.com>
2025-01-22 20:34:43 -0300

    SearchItems component sort

--- src/components/SearchItems.jsx
+++ src/components/SearchItems.jsx
@@ -10,7 +10,17 @@ export default function SearchItems() {
   const dispatch = useDispatch();
 
   const { searchResult } = useSelector((reducer) => reducer.theMetReducer.searchResultReducer);
-  const { pending: loading, objects: theMetObjects} = useSelector((reducer) => reducer.theMetReducer.objectsReducer);
+  const { pending: loadingTMO, fulfilled: fulfilledTMO, objects: theMetObjects} = useSelector((reducer) => reducer.theMetReducer.objectsReducer);
+
+  let halfSizeCounter = 0;
+  fulfilledTMO && theMetObjects.map((obj) => {
+    if (obj.orientation === "portrait")
+      halfSizeCounter = 3 - halfSizeCounter;
+    if (obj.orientation !== "portrait" && halfSizeCounter > 0) {
+      halfSizeCounter--;
+      obj.orientation = "half-landscape";
+    }
+  });
 
   const addObjects = (quantity) => {
     dispatch(theMetAddObjects(getUniqueRandom(theMetObjects, searchResult, quantity)));
@@ -20,26 +30,16 @@ export default function SearchItems() {
     addObjects(10);
   }, [searchResult]);
 
-  let halfSizeCounter = 0;
-
   const setLoadingCursor = () => {document.body.style.cursor = "wait";};
   const setDefaultCursor = () => {document.body.style.cursor = "default";};
 
   return (
     <>
       <div className="search-items">
-        {loading ? setLoadingCursor() : setDefaultCursor()}
-        {theMetObjects.map((obj) => {
-            if (obj.orientation === "portrait")
-              halfSizeCounter = 3 - halfSizeCounter;
-            if (obj.orientation !== "portrait" && halfSizeCounter > 0) {
-              halfSizeCounter--;
-              obj.orientation = "half-landscape";
-            }
-            return <CardObject key={obj.objectID} tmo={obj} />;
-          })}
+        {loadingTMO ? setLoadingCursor() : setDefaultCursor()}
+        {theMetObjects.map(obj => <CardObject key={obj.objectID} tmo={obj} />)}
       </div>
-      <button className="btn__add-more-objects" disabled={loading} onClick={() => addObjects(10)} >
+      <button className="btn__add-more-objects" disabled={loadingTMO} onClick={() => addObjects(10)} >
         Add More Objects
       </button>
     </>
