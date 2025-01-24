4fb3ba0f6d666817ee41347b7a4f8e437cb1c247
Fabio Marques <engfabiom@gmail.com>
2025-01-24 02:43:31 -0300

    fix: remove <ul> from <p> and minor html fixes.

--- src/components/WebsiteHeader.jsx
+++ src/components/WebsiteHeader.jsx
@@ -5,7 +5,7 @@ export default function WebsiteHeader() {
     <div className="website-header__container">
       <div className="website-header__text">
         <h1 className="website-header__text__title">
-        MetExp : The Met Museum Explorer
+          MetExp : The Met Museum Explorer
         </h1>
         <p className="website-header__text__descr">
           Welcome to <strong>MetExp</strong>, a React-based project designed to
@@ -15,15 +15,15 @@ export default function WebsiteHeader() {
           content.
         </p>
         <p className="website-header__text__descr">
-        <strong>MetExp</strong> is a learning-oriented project, aiming to:
-          <ul className="website-header__text__list">
-            <li>Demonstrate skills in <span>front-end</span> web development using <span>React</span> and <span>Redux</span>.</li>
-            <li>Showcase the ability to create dynamic, visually appealing web applications.</li>
-            <li>Provide a practical example of <span>state management</span> and <span>responsive design</span> implementation.</li>
-          </ul>
+          <strong>MetExp</strong> is a learning-oriented project, aiming to:
         </p>
+        <ul className="website-header__text__list">
+          <li>Demonstrate skills in <span>front-end</span> web development using <span>React</span> and <span>Redux</span>.</li>
+          <li>Showcase the ability to create dynamic, visually appealing web applications.</li>
+          <li>Provide a practical example of <span>state management</span> and <span>responsive design</span> implementation.</li>
+        </ul>
         <p className="website-header__text__descr">
-        Explore <strong>MetExp</strong> and experience the beauty of combining <strong>art</strong> with <strong>technology</strong>! 🎨💻📱
+          Explore <strong>MetExp</strong> and experience the beauty of combining <strong>art</strong> with <strong>technology</strong>! 🎨💻📱
         </p>
       </div>
     </div>
