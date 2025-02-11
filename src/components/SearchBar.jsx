d9a3c2891bb59f5bae4c0b5cc9b122d99507155e
Fabio Marques <engfabiom@gmail.com>
2025-02-11 01:37:10 -0300

    SearchBar: removal of useless 'all' query params, include departments filter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { apiSearch } from "../api/requests";
-import { useDispatch } from "react-redux";
+import { useDispatch , useSelector} from "react-redux";
 import {
   theMetCleanObjects,
   theMetSetSearchResult,
@@ -16,24 +16,26 @@ function SearchBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
+  const {data: departments} = useSelector(rootReducer=>rootReducer.theMetReducer.departmentsReducer);
+
   const [params, setParams] = useState({
-    q: "",
+    departmentId: "*",
     isHighlight: false,
     isOnView: false,
-    all: true,
     title: false,
     tags: false,
     artistOrCulture: false,
+    q: "",
   });
 
   function handleChange(event) {
     const { type, name, value, checked } = event.target;
     setParams((prev) => {
-      let result = {...prev }
-      if (type === "checkbox") result = { ...result, [name]: checked }
-      else if (type === "radio" ) result = { ...result, all: false, title: false, tags: false, artistOrCulture: false, [value]: checked }
-      else result = { ...result, [name]: value }
-      return result;
+      switch (type) {
+        case "checkbox": return { ...prev, [name]: checked };
+        case "radio": return { ...prev, title: false, tags: false, artistOrCulture: false, [value]: checked };
+        default: return { ...prev, [name]: value };
+      }
       });
   }
 
@@ -46,21 +48,19 @@ function SearchBar() {
       q: "",
       isHighlight: false,
       isOnView: false,
-      all: true,
       title: false,
       tags: false,
       artistOrCulture: false,  
     });
   }
 
-  // submit tag
   return (
     <div className="search-bar__container">
       <form onSubmit={newQ} className="search-bar__input-box">
-        <input autoComplete="off"  name="q" value={params.q} type="text" placeholder="type a keyword" className="search-bar__input-text" onChange={handleChange} />
-        <button type="sumit" className="search-bar__input-button"><IconMagnifyingGlass /></button>
+        <input autoComplete="off" name="q" value={params.q} type="text" placeholder="type a keyword" className="search-bar__input-text" onChange={handleChange} />
+        <button type="sumit" className="search-bar__input-button"> <IconMagnifyingGlass /> </button>
       </form>
-      <div className="search-bar__filters-box" onClick={toggleMenu}>
+      <div className="search-bar__filters-box" onClick={toggleMenu}> 
         <IconFilters />
         <span className="search-bar__filters-text">filters</span>
         <IconArrow isMenuOpen={isMenuOpen} />
@@ -68,32 +68,18 @@ function SearchBar() {
 
       {isMenuOpen && (
         <div className="dropdown-menu__container">
-          <label className="dropdown-menu__item">
-            <input name="isHighlight" checked={params.isHighlight} type="checkbox" className="dropdown-menu__checkbox" onChange={handleChange} />
-            Highlight of the Met
-          </label>
-          <label className="dropdown-menu__item">
-            <input name="isOnView" checked={params.isOnView} type="checkbox" className="dropdown-menu__checkbox" onChange={handleChange} />
-            Visible at the MET
-          </label>
+          <label className="dropdown-menu__item"> <input name="isHighlight" checked={params.isHighlight} type="checkbox" className="dropdown-menu__checkbox" onChange={handleChange} /> Highlight of the Met </label> 
+          <label className="dropdown-menu__item"> <input name="isOnView" checked={params.isOnView} type="checkbox" className="dropdown-menu__checkbox" onChange={handleChange} /> Visible at the MET </label>
+          <select name="departmentId" value={params.departmentId} onChange={handleChange}>
+            <option value="*">All departments</option>
+            {departments?.length && departments.map(d => <option key={d.departmentId} value={d.departmentId}>{d.displayName}</option>)}
+          </select>
           <fieldset>
             <legend>Search against:</legend>
-            <label className="dropdown-menu__item">
-              <input name="against" value="all" checked={params.all} type="radio" className="dropdown-menu__radio" onChange={handleChange} />
-              All Objects
-            </label>
-            <label className="dropdown-menu__item">
-              <input name="against" value="title" checked={params.title} type="radio" className="dropdown-menu__radio" onChange={handleChange} />
-              Titles
-            </label>
-            <label className="dropdown-menu__item">
-              <input name="against" value="tags" checked={params.tags} type="radio" className="dropdown-menu__radio" onChange={handleChange} />
-              Tags
-            </label>
-            <label className="dropdown-menu__item">
-              <input name="against" value="artistOrCulture" checked={params.artistOrCulture} type="radio" className="dropdown-menu__radio" onChange={handleChange} />
-              Artist or Culture
-            </label>
+            <label className="dropdown-menu__item"> <input name="against" checked={!params.title && !params.tags && !params.artistOrCulture} type="radio" className="dropdown-menu__radio" onChange={handleChange} /> All Objects </label>
+            <label className="dropdown-menu__item"> <input name="against" value="title" checked={params.title} type="radio" className="dropdown-menu__radio" onChange={handleChange} /> Titles </label>
+            <label className="dropdown-menu__item"> <input name="against" value="tags" checked={params.tags} type="radio" className="dropdown-menu__radio" onChange={handleChange} /> Tags </label>
+            <label className="dropdown-menu__item"> <input name="against" value="artistOrCulture" checked={params.artistOrCulture} type="radio" className="dropdown-menu__radio" onChange={handleChange} /> Artist or Culture </label>
           </fieldset>
         </div>
       )}
