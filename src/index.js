19a9891e9a6cc82f8207b3d3fff74a6d6a34382f
OrianeORDS <oriane.royon@gmail.com>
2025-02-16 17:13:24 +0100

    search for dragons

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { theMetSetSearchResult, theMetSetDepartments } from "./redux/rootActions
 console.clear();
 
 store.dispatch(theMetSetDepartments(apiGetDepartments()));
-store.dispatch(theMetSetSearchResult(apiSearch({isHighlight:true, hasImages:true, isOnView: true, q:"wolf"})));
+store.dispatch(theMetSetSearchResult(apiSearch({isHighlight: true, hasImages:true, isOnView: true, q:"dragon"})));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
