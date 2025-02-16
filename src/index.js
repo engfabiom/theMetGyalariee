e5de7a3c15a3fcb7f3f99533218b5f934317c649
OrianeORDS <oriane.royon@gmail.com>
2025-02-16 16:59:30 +0100

    first media query maxwidth-900px complete

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { theMetSetSearchResult, theMetSetDepartments } from "./redux/rootActions
 console.clear();
 
 store.dispatch(theMetSetDepartments(apiGetDepartments()));
-store.dispatch(theMetSetSearchResult(apiSearch({isHighlight:true, hasImages:true, isOnView: true, q:"hippos"})));
+store.dispatch(theMetSetSearchResult(apiSearch({isHighlight:true, hasImages:true, isOnView: true, q:"wolf"})));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
